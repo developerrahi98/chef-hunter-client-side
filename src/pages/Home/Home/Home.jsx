32ecaec9fe88a8b71df2cbba32e22bddb14887fb
asifrahi0 <asifahemmedrahi0@gmail.com>
2023-05-05 15:49:44 +0600

    footer added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -36,12 +36,39 @@ const Home = () => {
       <div className="mt-4">
         <h2 className="fw-bold text-center fs-1">All Chefs Here</h2>
         <div className="row row-cols-2 g-0 w-75 mx-auto">
-        {
-            chefs.map(chef => <Chefs 
-            key={chef.id}
-            chef={chef}
-            ></Chefs>)
-        }
+          {chefs.map((chef) => (
+            <Chefs key={chef.id} chef={chef}></Chefs>
+          ))}
+        </div>
+      </div>
+      <div className="d-flex mt-4 p-4 w-75 mx-auto justify-content-around">
+        <div>
+          <h1 className="fw-bold fs-1">Our Best Chefs</h1>
+          <p>
+            Our mission is to serve the community by offering a state-of-the-art
+            facility with professional management, and by hosting local events,
+            conventions, trade shows, meetings, and concerts generating
+            significant economic impact for the City of Pembroke Pines as well
+            as promoting education, entertainment and community pride, in a
+            fiscally responsible manner. In addition, the City Center will
+            support and encourage actions and programs that enhance the
+            desirability and attractiveness of Pembroke Pines to neighboring
+            communities.
+          </p>
+          <h1 className="fw-bold">Responsibilities</h1>
+          <ul>
+            <li>
+              Set up the kitchen with cooking utensils and equipment, like
+              knives, pans and kitchen scales
+            </li>
+            <li>Study each recipe and gather all necessary ingredients</li>
+            <li>Cook food in a timely manner</li>
+            <li>Ensure appealing plate presentation</li>
+            <li>Supervise Cooks and assist as needed</li>
+          </ul>
+        </div>
+        <div className="ms-4 mt-5">
+            <img className="rounded" src="/src/assets/images/istockphoto-1408797987-170667a.jpg" alt="" />
         </div>
       </div>
     </div>
--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
 const Footer = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <footer className="bg-dark text-light p-5">
+        <Container className="mx-auto">
+          <Row>
+            <Col md={6}>
+              <h4>Contact Us</h4>
+              <ul className="list-unstyled">
+                <li>123 Main St.</li>
+                <li>New York, NY 10001</li>
+                <li>(123) 456-7890</li>
+                <li>info@restaurant.com</li>
+              </ul>
+            </Col>
+            <Col md={6}>
+              <h4>Follow Us</h4>
+              <ul className="list-inline">
+                <li>
+                  <a href="#" className="text-decoration-none fw-bold">Facebook</a>
+                </li>
+                <li>
+                  <a href="#" className="text-decoration-none fw-bold">Twitter</a>
+                </li>
+                <li>
+                  <a href="#" className="text-decoration-none fw-bold">Instagram</a>
+                </li>
+              </ul>
+            </Col>
+          </Row>
+          <hr className="bg-light" />
+          <Row>
+            <Col md={12}>
+              <p className="text-center">
+                &copy; 2023 Restaurant. All rights reserved.
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
